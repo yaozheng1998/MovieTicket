030cf28cea8a6cac9bcb380be2278e7127b8b4fb
YZ <1150681747@qq.com>
2018-04-08 21:36:33 +0800

    Docker

--- microservice-hystrix-turbine/src/main/java/com/yz/TurbineApplication.java
+++ microservice-hystrix-turbine/src/main/java/com/yz/TurbineApplication.java
@@ -1,9 +1,14 @@
 package com.yz;
 
+import org.springframework.boot.SpringApplication;
+
 /**
  * @Author YZ
  * @Date 2018/3/31
  */
 
 public class TurbineApplication {
+    public static void main(String[]args){
+        SpringApplication.run(TurbineApplication.class,args);
+    }
 }
